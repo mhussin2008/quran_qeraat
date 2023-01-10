6b940cc373da2fa9db410b1188a662e2d4ae7964
mohamed hussin <65326535+mhussin2008@users.noreply.github.com>
2023-01-10 17:31:25 +0200

    faces page working added mp3

--- lib/faces_page.dart
+++ lib/faces_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quran_qeraat/main.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
 
 class facesPage extends StatefulWidget {
   const facesPage({Key? key}) : super(key: key);
@@ -15,14 +16,24 @@ class _facesPageState extends State<facesPage> {
       body: SafeArea(
         child: Center(child: Column(
           children:
-          faces.map((e) => Container(child: Column(
-            children: [
-              Text(e['text'],
-              style: TextStyle(color: Colors.blue,fontFamily: 'qaloon',fontSize: 20,),textDirection: TextDirection.rtl,),
-              Text(e['desc'],
-                  style: TextStyle(color: Colors.red,fontFamily: 'qaloon',fontSize: 20)),
-              SizedBox(height: 20,)
-            ],
+          faces.map((e) => Container(child: GestureDetector(
+            onTap: (){
+              AssetsAudioPlayer.newPlayer().open(
+                Audio("assets/audio/1-6-1.mp3"),
+                autoStart: false,
+                showNotification: true,
+              );
+
+            },
+            child: Column(
+              children: [
+                Text(e['text'],
+                style: TextStyle(color: Colors.blue,fontFamily: 'qaloon',fontSize: 20,),textDirection: TextDirection.rtl,),
+                Text(e['desc'],
+                    style: TextStyle(color: Colors.red,fontFamily: 'qaloon',fontSize: 20)),
+                SizedBox(height: 20,)
+              ],
+            ),
           ),)).toList()
           ,
         ),
