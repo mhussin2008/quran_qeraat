82beb48b40b45c8db2722312ddc86dbc825b788e
mohamed hussin <65326535+mhussin2008@users.noreply.github.com>
2023-01-11 16:53:16 +0200

    working audio ok numbered faces

--- lib/faces_page.dart
+++ lib/faces_page.dart
@@ -13,34 +13,87 @@ class facesPage extends StatefulWidget {
 class _facesPageState extends State<facesPage> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: SafeArea(
-        child: Center(child: Column(
-          children:
-          faces.map((e) => Container(child: GestureDetector(
-            onTap: (){
-              dev.log(e['face'].toString());
-              AssetsAudioPlayer.newPlayer().open(
-                Audio("assets/audio/1-6-"+e['face'].toString()+".mp3"),
-                autoStart: true,
-                showNotification: false,
-              );
+    return Scaffold(
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Center(
+             child: Padding(
+               padding: const EdgeInsets.all(8.0),
+               child: Column(
+                    children: faces
+                      .map((e) => Column(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.all(15),
+                              decoration: BoxDecoration(
+                                  border:
+                                      Border.all(color: Colors.teal, width: 5),
+                                  borderRadius: BorderRadius.all(Radius.circular(20))),
+                              child: GestureDetector(
+                                onTap: () {
+                                  dev.log(e['face'].toString());
+                                  AssetsAudioPlayer.newPlayer().open(
+                                    Audio("assets/audio/1-6-" +
+                                        e['face'].toString() +
+                                        ".mp3"),
+                                    autoStart: true,
+                                    showNotification: false,
+                                  );
+                                },
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      e['text'],
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.blueAccent,
+                                        fontFamily: 'qaloon',
+                                        fontSize: 20,
+                                      ),
+                                      textDirection: TextDirection.rtl,
+                                    ),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: [
+                                        Text(e['desc'],
+                                            textAlign: TextAlign.center,
+                                            style: TextStyle(
+                                                color: Colors.red,
+                                                fontFamily: 'qaloon',
+                                                fontSize: 20)),SizedBox(width: 10,)
+                                      , Container(
+                                          height: 40,
+                                          width: 40,
+                                          decoration: BoxDecoration(
+                                            border: Border.all(color: Colors.teal, width: 5),
+                                            borderRadius: BorderRadius.all(Radius.circular(20))),
+                                          child: Text(e['face'].toString(),
+                                          style: TextStyle(fontSize: 22)
+                                          ,textAlign: TextAlign.center,)
 
-            },
-            child: Column(
-              children: [
-                Text(e['text'],
-                style: TextStyle(color: Colors.blue,fontFamily: 'qaloon',fontSize: 20,),textDirection: TextDirection.rtl,),
-                Text(e['desc'],
-                    style: TextStyle(color: Colors.red,fontFamily: 'qaloon',fontSize: 20)),
-                SizedBox(height: 20,)
-              ],
+                              )],
+                                    ),
+                                    //SizedBox(height: 20,)
+                                  ],
+                                ),
+                              )
             ),
-          ),)).toList()
-          ,
+                        ]+
+                            [
+                              Container(
+                                  color: Colors.red,
+                                  child: SizedBox(
+                                    height: 20,
+                                  )
+                              )
+                            ],
+                      )
+            ).toList()
         ),
+             ),
       ),
-    )
-    );
+          ),
+    ));
   }
 }
